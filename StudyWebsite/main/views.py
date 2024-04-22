4b7de1cfa76ef952e0af00723c4849aabdf4ee0e
waad <waad.f.alanzi@gmail.com>
2024-04-22 14:42:57 +0300

    study group created

--- StudyWebsite/main/views.py
+++ StudyWebsite/main/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-
+from .models import StudyGroup 
 
 # Create your views here.
 def index_view(request: HttpRequest):
@@ -16,3 +16,33 @@ def group_dashboard(request:HttpRequest):
 def user_dashboard(request:HttpRequest):
 
   return render(request,"main/user_dashboard.html")
+
+
+
+
+def create_group(request:HttpRequest):
+  if request.method =="POST":
+    try:
+     new_group=StudyGroup(
+      creator=request.user,
+      name=request.POST["name"],
+      description=request.POST["description"],
+      icon=request.FILES.get("icon"),
+            )
+     new_group.save()
+    except Exception as e:
+            print(e)
+    return redirect('main:user_dashboard_view')
+
+  return render(request,"main/create_group.html")
+
+
+def delete_group(request:HttpRequest , group_id):
+  try:
+    group=StudyGroup.objects.get(pk=group_id)
+    group.delete()
+  except StudyGroup.DoesNotExist:
+    group=None
+  except Exception as e:
+    print(e)
+  return redirect('main:user_dashboard_view')
