fdc7e4491b17f56c96237f7fc51b83d2dfdc6678
AsmaaFelemban <Asmaakf@hotmail.com>
2024-04-25 11:10:55 +0300

    worked on review_sets view

--- StudyWebsite/review_sets/urls.py
+++ StudyWebsite/review_sets/urls.py
@@ -5,7 +5,9 @@ app_name  = "review_sets"
 
 urlpatterns = [
     path("add/set/", views.add_set_view, name="add_set_view"),
-    path("all/sets/<group_id>", views.all_sets_view, name="all_sets_view"),
+    path("all/sets/", views.all_sets_view, name="all_sets_view"),
+    path("update/set/<set_id>/", views.update_set_view, name="update_set_view"),
+    path("full/set/<set_id>/", views.full_set_view, name="full_set_view"),
+    path("delete/set/<set_id>/", views.delete_set_view, name="delete_set_view"),
     path("add/card/", views.add_card_view, name="add_card_view"),
-    path("all/set/cards", views.all_set_cards_view, name="all_set_cards_view"),
 ]
--- StudyWebsite/review_sets/views.py
+++ StudyWebsite/review_sets/views.py
@@ -9,42 +9,61 @@ from main.models import StudyGroup
 
 
 # Review Set views
-def add_set_view(request: HttpRequest, group_id):
+def add_set_view(request: HttpRequest):
 
     if request.method == 'POST':
 
         if request.user.is_authenticated:
             try:
-                group = StudyGroup.objects.get(pk=group_id)
                 new_set = ReviewSet(
                     title = request.POST["title"],
                     description = request.POST["description"],
                     created_by = request.user,
-                    group = group
                 )
-
                 new_set.save()  
-                return redirect("review_sets:all_sets_view", group_id=group.id) ##pop up successfully
+                return redirect("review_sets:all_sets_view") ##pop up successfully
             except Exception as e:
                 print(e)
-
     return render(request, "review_sets/add_set.html")
 
 
 
-def update_set_view(request: HttpRequest):
-    pass
+def update_set_view(request: HttpRequest, set_id):
 
-def delete_set_view(request: HttpRequest):
-    pass
+    #limit access to this view for only staff
 
-def all_sets_view(request: HttpRequest, group_id):
+    #update
+    r_set = ReviewSet.objects.get(pk=set_id)
+    if request.method == "POST":
+        try:
+            r_set.title = request.POST["title"]
+            r_set.description = request.POST["description"]
+            r_set.save()
+            return redirect("review_sets:full_set_view", set_id = r_set.id)
+        except Exception as e:
+            print(e)
+    return render(request, "review_sets/update_set.html", {"r_set" : r_set})
+
+
+def delete_set_view(request: HttpRequest, set_id ):
+
+    #limit access to this view for only staff
+   
+    try:
+        r_set = ReviewSet.objects.get(pk=set_id)
+        r_set.delete()
+    except Exception as e:
+        print(e)
+    return redirect("review_sets:all_sets_view")
+
+
+def all_sets_view(request: HttpRequest):
     sets= ReviewSet.objects.all()
-    group=StudyGroup.objects.get(pk=group_id)
-    return render(request, "review_sets/all_sets.html", {"sets": sets , "group":group} )
+    return render(request, "review_sets/all_sets.html", {"sets": sets } )
 
 
 
+####################### FlashCard views ##########################
 
 # FlashCard views
 def add_card_view(request: HttpRequest):
@@ -57,7 +76,7 @@ def add_card_view(request: HttpRequest):
                 answer = request.POST["answer"],
             )
             new_card.save()  
-            return redirect("review_sets:all_set_cards_view") ##pop up successfully
+            return redirect("review_sets:all_cards_view") ##pop up successfully
         except Exception as e:
             print(e)
 
@@ -70,9 +89,9 @@ def update_card_view(request: HttpRequest):
 def delete_card_view(request: HttpRequest):
     pass
 
-def all_set_cards_view(request: HttpRequest):
+def full_set_view(request: HttpRequest): # containe set detail
 
     cards = FlashCard.objects.all()
 
-    return render(request, "review_sets/all_set_cards.html", {"cards" : cards})
+    return render(request, "review_sets/full_set.html", {"cards" : cards})
 
