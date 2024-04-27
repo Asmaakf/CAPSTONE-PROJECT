d216e854b57962c38dff8c837f32975c15b70990
AsmaaFelemban <Asmaakf@hotmail.com>
2024-04-27 17:56:51 +0300

    update

--- StudyWebsite/common_questions/urls.py
+++ StudyWebsite/common_questions/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name  = "common_questions"
 
 urlpatterns = [
-
-    path('support-question-answer/', views.support_question_answer, name='support_question_answer'),  
+    path('support_question_answer/', views.support_question_answer, name='support_question_answer'),  
     path('view-messages/', views.view_messages, name='view_messages'),  
     path('delete_question/<int:pk>/', views.delete_question, name='delete_question'),  
     path('edit_question/<int:pk>/', views.edit_question, name='edit_question'),  
